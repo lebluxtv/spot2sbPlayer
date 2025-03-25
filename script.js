9a155d27f1ef6375d38c6c48d3d47d82c27374b4
lebluxtv <136271820+lebluxtv@users.noreply.github.com>
2025-03-25 19:35:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -554,7 +554,7 @@ function handlePopupDisplay() {
   const totalDuration = popupDurationSec * 1000;
   const phase1Duration = totalDuration * 0.6; // fade out background, title, artist, progress bar
   const phase2Duration = totalDuration * 0.4; // fade out requester info (si présent)
-  const phase3Duration = totalDuration * 0.4; // fade out album art
+  const phase3Duration = totalDuration * 0.2; // fade out album art
 
   const player = document.getElementById('player');
   const bgBlur = document.getElementById('bg-blur');
